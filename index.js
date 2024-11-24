2c315937b0b4248c6b8da7dc0d061f20e54977b9
harricePrivat <briceprivat292@gmail.com>
2024-11-24 09:29:41 +0300

    add student done

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app= express()
 const sequelize= require('./database')
 const User= require('./models/User')
 const router= require('./routes/User')
+const path= require('path')
 
-
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use(express.json())
 app.use(router)
--- routes/User.js
+++ routes/User.js
@@ -1,6 +1,7 @@
 const express= require('express')
 const router = express.Router()
 const multer = require('multer')
+const path = require('path')
 const User= require('../models/User')
 
 
@@ -10,11 +11,11 @@ const storage = multer.diskStorage({
       cb(null, path.join(__dirname, "../images"));
     },
     filename: (req, file, cb) => {
-      // Utiliser Req.titre comme nom de fichier
-      const fileName = req.body.titre; // Utilisation de Req.titre pour le nom
-      const timestamp = new Date().toISOString().replace(/[-:.]/g, "");
+      const fileName = req.body.nom;
+      const lastName=req.body.prenom
+      const niveau=req.body.niveau 
       const fileExtension = path.extname(file.originalname); // Extension du fichier
-      const newFileName = `${fileName}_${timestamp}${fileExtension}`; // Exemple : "Titre_20231124083000.jpg"
+      const newFileName = `${fileName}_${lastName}_${niveau}${fileExtension}`; // Exemple : "Titre_20231124083000.jpg"
   
       cb(null, newFileName); // Retourner le nouveau nom de fichier
     },
@@ -25,36 +26,46 @@ const storage = multer.diskStorage({
   // Utiliser le middleware upload.single pour gérer un seul fichier
   router.post("/create-user", upload.single("image"), async (req, res) => {
      const Req = req.body;
-    console.log(Req)
-     // try {
-    //   if (Req.nom && Req.prenom && Req.naissance && Req.niveau) {
-    //     let result
-    //     if(Req.cin=="no"){
-    //         result = await User.create({
-    //             nom: Req.nom,
-    //             prenom: Req.prenom,
-    //             date_naissace: Req.naissance,
-    //             niveau: Req.niveau,
-    //             numero_cin: Req.cin,
-    //             path_images: `http://192.168.43.41:3000/Images/${req.file.filename}`,
-    //           });
-    //     }else{
-    //         result = await User.create({
-    //             nom: Req.nom,
-    //             prenom: Req.prenom,
-    //             naissance: Req.naissance,
-    //             niveau: Req.niveau,
-    //             path_images: `http://192.168.43.41:3000/Images/${req.file.filename}`,
-    //           });
-    //     }  
+     console.log(Req.nom , Req.prenom, Req.naissance,Req.niveau,Req.cin)
+     try {
+      if (Req.nom && Req.prenom && Req.naissance && Req.niveau) {
+        let result
+        if(Req.cin=="no"){
+          try{
+            result = await User.create({
+                nom: Req.nom,
+                prenom: Req.prenom,
+                date_naissace: Req.naissance,
+                niveau: Req.niveau,
+                numero_cin: Req.cin,
+                path_images: `http://192.168.43.41:3000/Images/${req.file.filename}`,
+              });
+          }catch(error){
+            console.log("Voici l'erreur",error)
+          }
+        }else{
+            try{
+                result = await User.create({
+                    nom: Req.nom,
+                    prenom: Req.prenom,
+                    date_naissace: Req.naissance,
+                    niveau: Req.niveau,
+                    numero_cin: Req.cin,
+                    path_images: `http://192.168.43.41:3000/Images/${req.file.filename}`,
+                  });
+            }catch(e){
+                console.log("Voici l'erreur",e)
+            }
+          
+        }  
          
-    //     res.status(200).json({ message: "Publication créée avec succès" });
-    //   } else {
-    //     res.status(400).json({ error: "Champ vide impossible de créer la publication" });
-    //   }
-    // } catch (error) {
-    //   res.status(500).json({ error: error.message });
-    // }
+        res.status(200).json({ message: "Publication créée avec succès" });
+      } else {
+        res.status(400).json({ error: "Champ vide impossible de créer la publication" });
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
   });
   
 
